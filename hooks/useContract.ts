76e901c2d4e2ab4791c2a94164ee967d342a0729
jingyi2811 <jingyi2811@gmail.com>
2025-12-17 10:34:56 +0800

    Add ERC-1155 contract support with batch balance hooks

--- hooks/useContract.ts
+++ hooks/useContract.ts
@@ -1,8 +1,8 @@
 import { useMemo } from 'react';
 import { Abi, WalletClient } from 'viem';
 import { getContract } from '../utils/getContract';
-import { babtABI, collabABI } from '../abis';
-import { Address, useContractRead, useNetwork, usePublicClient, useWalletClient } from 'wagmi';
+import { babtABI, collabABI, erc1155ABI } from '../abis';
+import { Address, useContractRead, useContractReads, useNetwork, usePublicClient, useWalletClient } from 'wagmi';
 import { BABT_ADDRESSES, COLLAB_ADDRESS } from '../constants/addresses';
 
 export function useContract<TAbi extends Abi>(address?: Address, abi?: TAbi, chainId?: number) {
@@ -40,4 +40,78 @@ export function useBABTBalanceOf({ address }: { address?: Address }) {
 
 export function useCollabContract() {
   return useContract(COLLAB_ADDRESS, collabABI);
+}
+
+export function useERC1155Contract(contractAddress?: Address) {
+  return useContract(contractAddress, erc1155ABI);
+}
+
+export function useERC1155BalanceOf({
+  contractAddress,
+  account,
+  tokenId,
+}: {
+  contractAddress?: Address;
+  account?: Address;
+  tokenId?: bigint;
+}) {
+  return useContractRead({
+    address: contractAddress,
+    abi: erc1155ABI,
+    functionName: 'balanceOf',
+    args: account && tokenId !== undefined ? [account, tokenId] : undefined,
+    enabled: !!contractAddress && !!account && tokenId !== undefined,
+  });
+}
+
+export function useERC1155BalanceOfBatch({
+  contractAddress,
+  accounts,
+  tokenIds,
+}: {
+  contractAddress?: Address;
+  accounts?: Address[];
+  tokenIds?: bigint[];
+}) {
+  return useContractRead({
+    address: contractAddress,
+    abi: erc1155ABI,
+    functionName: 'balanceOfBatch',
+    args: accounts && tokenIds ? [accounts, tokenIds] : undefined,
+    enabled: !!contractAddress && !!accounts?.length && !!tokenIds?.length && accounts.length === tokenIds.length,
+  });
+}
+
+export function useERC1155Uri({
+  contractAddress,
+  tokenId,
+}: {
+  contractAddress?: Address;
+  tokenId?: bigint;
+}) {
+  return useContractRead({
+    address: contractAddress,
+    abi: erc1155ABI,
+    functionName: 'uri',
+    args: tokenId !== undefined ? [tokenId] : undefined,
+    enabled: !!contractAddress && tokenId !== undefined,
+  });
+}
+
+export function useERC1155IsApprovedForAll({
+  contractAddress,
+  account,
+  operator,
+}: {
+  contractAddress?: Address;
+  account?: Address;
+  operator?: Address;
+}) {
+  return useContractRead({
+    address: contractAddress,
+    abi: erc1155ABI,
+    functionName: 'isApprovedForAll',
+    args: account && operator ? [account, operator] : undefined,
+    enabled: !!contractAddress && !!account && !!operator,
+  });
 }
